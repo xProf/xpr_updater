b02bfc3fda2a8d627ce14ae8d93e6c6b396cf47a
xProf <xProf@Kaa>
2012-08-16 01:33:52 +0300

    почти готов

--- src/xpr_updater/Xpr_updater.java
+++ src/xpr_updater/Xpr_updater.java
@@ -4,8 +4,11 @@
  */
 package xpr_updater;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Label;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -19,59 +22,122 @@ import javax.swing.JFrame;
  *
  * @author xProf
  */
-public class Xpr_updater {
-        
-//FileOutputStream fos;
-//HttpURLConnection urlConnect;
-//BufferedInputStream inStream;
+public class Xpr_updater 
+
+{
+    
+    public static String mineFolder = "minecraft2";    
+    public static String downLoadURL="http://minecraft-tyachiv.org.ua/download/";
+    public static String launcherFileName="minecraft.exe";
+    public static Label labelMain,labelText,labelBar1,labelBar2;
+    
+    
+    public static void Run() throws MalformedURLException, IOException
+    {
+    openWindow();    
+    downLoad();
+    if (new File(getMineDirectory()+File.separator+launcherFileName).exists() )Runtime.getRuntime().exec(getMineDirectory()+File.separator+launcherFileName);
+    System.exit(0);
+    }
     
     public static void openWindow() throws MalformedURLException, IOException 
     {
-       JFrame okno=new JFrame("Test");
-       okno.setLocation(0, 0);
+       JFrame okno=new JFrame("Minecraft Updater 0.01");
+       okno.setLocation(50, 50);
        okno.setSize(300, 200);
+       okno.setResizable(false);
+       okno.setLayout(new BorderLayout());
        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       Label label;
-       long vsego=0;
        okno.setVisible(true);
+       labelMain=new Label();
+       labelText=new Label();
+       labelBar1=new Label();
+       labelBar2=new Label();
        
-       byte[] b =new byte[1024];
-       int count;
-            label=new Label("ras ras ras");
-            okno.add(label);
-            label.setLocation(10,10);
-            label.setSize(200, 30);
-            label.setVisible(true);
+       labelMain.setBackground(Color.decode("#440044"));
+       labelMain.setForeground(Color.decode("#ffffff"));
+       labelMain.setAlignment(1);
+       labelMain.setLocation(0,80);
+       labelMain.setSize(300, 120);
+       labelMain.setVisible(true);
+       
+       labelText.setBackground(Color.decode("#440044"));
+       labelText.setForeground(Color.decode("#ffffff"));
+       labelText.setAlignment(1);
+       labelText.setLocation(0,0);
+       labelText.setSize(300, 80);
+       labelText.setVisible(true);
+       
+       labelBar1.setBackground(Color.decode("#777700"));
+       labelText.setLocation(0,0);
+       labelText.setSize(260, 20);
+       labelText.setVisible(true);
+       okno.add(labelMain);
+       okno.add(labelText);
+       okno.add(labelBar1);
+       okno.add(labelBar2);
+   }
+    
+    public static void downLoad() 
+    {        
+        try {
             FileOutputStream fos;
-            URL url = new URL("http://minecraft-tyachiv.org.ua/download/minecraft.jar");
-            long size=url.getFile().length();
+            long vsego=0;       
+            byte[] b =new byte[1024];
+            int count;
+            
+            URL url = null;
+             
+            try {
+                 url = new URL(downLoadURL+launcherFileName);
+             } catch (MalformedURLException ex) {
+                 Logger.getLogger(Xpr_updater.class.getName()).log(Level.SEVERE, null, ex);
+             }
+            
             URLConnection urlConnect=url.openConnection();
             BufferedInputStream inStream=new BufferedInputStream(urlConnect.getInputStream());
 
- 
-                fos = new FileOutputStream("d:/_musor/minecraft.jar");
+            
+            long size=urlConnect.getContentLength();
+            File mineDir=new File(getMineDirectory());
+            if (!mineDir.exists()) mineDir.mkdir();
+            
+            fos = new FileOutputStream(getMineDirectory()+File.separator+launcherFileName);
+            while((count=inStream.read(b))!=-1)
+               {
+                  fos.write(b, 0, count);
+                  vsego=vsego+count;
+                  labelText.setText("Updating "+(100*vsego/size)+"%");
+               }    
+            fos.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Xpr_updater.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-                while((count=inStream.read(b))!=-1)
-                {
-                    fos.write(b, 0, count);
-                    vsego=vsego+count;
-                    label.setText(" "+(size));
-                }    
-                fos.close();
-
-
-        
+            
     }
-    /**
-     * @param args the command line arguments
-     */
+    
+    
+    private static String getMineDirectory()
+            {
+                String OS = System.getProperty("os.name").toUpperCase();
+                if (OS.contains("WIN"))
+                    return System.getenv("APPDATA")+"\\."+mineFolder;
+                else if (OS.contains("MAC"))
+                       return System.getProperty("user.home") + "/Library/Application " + "Support";
+                else if (OS.contains("NUX"))
+                        return System.getProperty("user.home")+"/."+mineFolder;
+                return System.getProperty("user.dir");
+            }
+    
+    
     public static void main(String[] args) {
              javax.swing.SwingUtilities.invokeLater(new Runnable() 
                 {
                     public void run() 
                         {
                         try {
-                            openWindow();
+                            Run();
                         } catch (MalformedURLException ex) {
                             Logger.getLogger(Xpr_updater.class.getName()).log(Level.SEVERE, null, ex);
                         } catch (IOException ex) {
